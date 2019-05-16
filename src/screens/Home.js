d5950dee7cf4b1383f786358dd956f988e855a0c
Eric Guzmán <ericmguzmanc@gmail.com>
2019-05-16 19:24:18 -0400

    some changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { View, Text, StatusBar, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StatusBar, StyleSheet, TextInput, Button } from 'react-native';
 
 import { Fonts } from '../utils/fonts';
 
@@ -9,11 +9,14 @@ class HomeScreen extends PureComponent {
     placeName: "",
     isFocused: true
   };
+
+  // status #6200EE
+  // header #7C4DFF
   
   static navigationOptions = {
     title: "Home",
     headerStyle: {
-      backgroundColor: '#7C4DFF',
+      backgroundColor: '#6200EE',
     },
     headerTintColor: '#FFFFFF',
     headerTitleStyle: {
@@ -35,21 +38,29 @@ class HomeScreen extends PureComponent {
   render() {
     return(
      <View style={styles.container}>
-        <StatusBar backgroundColor="#7C4DFF" barStyle="light-content" />
-        <Text style={styles.welcome}>This is the HomeScreen!</Text>
-        <TextInput 
-          style={[styles.placeInput, {
-            borderBottomColor: this.state.isFocused
-              ? 'pink'
-              : 'red',
-          }]}
-          onChangeText={this.placeNameChangedHandler}
-          value={this.state.placeName}
-          placeholder="Place Input"
-          placeholderColor="gray"
-          onFocus={this.handleFocus}
-          onBlur={this.handleBlur}
-        />
+        <StatusBar backgroundColor="#3700B3" barStyle="light-content" />
+        <View style={styles.inputContainer}>
+
+          <TextInput 
+            style={styles.placeInput}
+            onChangeText={this.placeNameChangedHandler}
+            value={this.state.placeName}
+            placeholder="Place Input"
+            placeholderColor="gray"
+            onFocus={this.handleFocus}
+            onBlur={this.handleBlur}
+            selectionColor="#6200EE"
+            underlineColorAndroid="#7C4DFF"
+            
+          />
+          <Button 
+            styles={styles.placeButton}
+            title="Add"
+            color="#03DAC5"
+            Fonts={Fonts.OpenSans}
+            
+          /> 
+        </View>
       </View>
     );
   }
@@ -58,34 +69,28 @@ class HomeScreen extends PureComponent {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
+    padding: 15,
     backgroundColor: '#FFFFFF',
-    borderBottomWidth: 0.3,
-    borderColor: 'lightgray',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: 'black',
-    fontFamily: Fonts.OpenSansBold
+  inputContainer: {
+    // flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
   placeInput: {
-    margin: 15,
-    flexDirection: 'column',
-    height: 52,
-    paddingLeft: 15,
-    borderWidth: 1,
-    borderTopLeftRadius: 5,
-    borderTopRightRadius: 5,
-    backgroundColor: 'lightgray',
+    // margin: 15,
+    // flexDirection: 'column',
+    width: "70%",
+    // paddingLeft: 15,
     fontFamily: Fonts.OpenSans,
-    borderColor: 'lightgray',
-    // borderTopColor: 'lightgray',
-    // borderLeftColor: 'lightgray',
-    // borderRightColor: 'lightgray',
-    // borderBottomColor: 'black',
+  },
+  placeButton: {
+    width: "30%",
+    color: "black"
   }
   
 });
