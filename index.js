eceedca27d11d8c643d9f560b4c75f79788a8b7a
Eric Guzmán <ericmguzmanc@gmail.com>
2019-07-13 16:02:56 -0400

    redux installation and configuration

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import { Provider } from 'react-redux';
 
-AppRegistry.registerComponent(appName, () => App);
+import store from './src/store';
+
+const ProviderContainer = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ProviderContainer);
