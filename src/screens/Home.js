f39fdfd85ce5dcfbf40e581ebb94fad7b51d971d
Eric Guzmán <ericmguzmanc@gmail.com>
2019-07-16 21:43:14 -0400

    redux integration

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,6 @@
 import React, { PureComponent } from "react";
+import { connect } from 'react-redux';
+
 import {
   View,
   StatusBar,
@@ -9,12 +11,15 @@ import {
 import { Fonts } from "../utils/fonts";
 import { PlaceInput, PlaceList, PlaceDetail } from "../components";
 
+import { addPlace, deletePlace, deselectPlace, selectPlace } from '../store/actions/index';
+
+
 class HomeScreen extends PureComponent {
 
-  state = {
-    places: [],
-    selectedPlace: null
-  };
+  // state = {
+  //   places: [],
+  //   selectedPlace: null
+  // };
 
   // status #6200EE
   // header #7C4DFF
@@ -32,42 +37,46 @@ class HomeScreen extends PureComponent {
   };
 
   placeAddedHandler = placeName => { 
-    this.setState( prevState => {
-      return {
-        places: prevState.places.concat({
-          key: Math.random().toString(), 
-          name: placeName,
-          image: {
-            uri: "https://www.westsideintltravel.com/wp-content/uploads/2015/04/germany-frankfort.jpg"
-          }
-        })
-      }
-    });
+    // this.setState( prevState => {
+    //   return {
+    //     places: prevState.places.concat({
+    //       key: Math.random().toString(), 
+    //       name: placeName,
+    //       image: {
+    //         uri: "https://www.westsideintltravel.com/wp-content/uploads/2015/04/germany-frankfort.jpg"
+    //       }
+    //     })
+    //   }
+    // });
+    this.props.onAddPlace(placeName);
   };
 
   placeSelectedHandler = key => {
-    this.setState(prevState => {
-      return {
-        selectedPlace: prevState.places.find(place => place.key === key)
-      }
-    })
+    // this.setState(prevState => {
+    //   return {
+    //     selectedPlace: prevState.places.find(place => place.key === key)
+    //   }
+    // });
+    this.props.onSelectPlace(key);
   };
-
+  
   placeDeletedHandler = () => {
-     this.setState(prevState => {
-      return {
-        places: prevState.places.filter( place => {
-          return place.key !== prevState.selectedPlace.key
-        }),
-        selectedPlace: null
-      }
-    });
+    //  this.setState(prevState => {
+      //   return {
+        //     places: prevState.places.filter( place => {
+          //       return place.key !== prevState.selectedPlace.key
+          //     }),
+          //     selectedPlace: null
+          //   }
+          // });
+    this.props.onDeletePlace();
   }
 
   modalClosedHandler = () => {
-    this.setState({
-      selectedPlace: null
-    });
+    // this.setState({
+    //   selectedPlace: null
+    // });
+    this.props.onDeselectPlace();
   }
 
   render() {
@@ -76,13 +85,13 @@ class HomeScreen extends PureComponent {
         <StatusBar backgroundColor="#3700B3" barStyle="light-content" />
 
         <PlaceDetail 
-          selectedPlace={this.state.selectedPlace} 
+          selectedPlace={this.props.selectedPlace} 
           onItemDeleted={this.placeDeletedHandler} 
           onModalClosed={this.modalClosedHandler}
           />
 
         <PlaceInput onPlaceAdded={this.placeAddedHandler} /> 
-        <PlaceList places={this.state.places} onItemSelected={this.placeSelectedHandler}/>
+        <PlaceList places={this.props.places} onItemSelected={this.placeSelectedHandler}/>
       </View>
     );
   }
@@ -98,4 +107,21 @@ const styles = StyleSheet.create({
   },
 });
 
-export default HomeScreen;
+
+const mapStateToProps = state => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: (name) => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: (key) => dispatch(selectPlace(key)),
+    onDeselectPlace: () => dispatch(deselectPlace())
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import placesReducer from './places';
 
 const rootReducer = combineReducers({
   places: placesReducer
